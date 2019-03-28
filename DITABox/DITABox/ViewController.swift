2c22fc866a9ddc51bcbf1a7dac41ffee7302aea1
MartianTribe <steve@martiantribe.com>
2019-03-28 09:52:16 -0400

    Added XCode Project

---
+++ DITABox/DITABox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DITABox
+//
+//  Created by Steven Suranie on 3/28/19.
+//  Copyright © 2019 Steven Suranie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
